36cb611932f4922990ed77662f98fbd4f8f01a3b
70747H <hussien.fcih2016@gmail.com>
2023-03-29 11:16:43 +0200

    add pre commit hook

--- src/app/role/role.model.js
+++ src/app/role/role.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const roleSchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true },
-  permissions: { type: [String], unique: true },
+  permissions: { type: [String], unique: true }
 })
 
 const Role = mongoose.model('roles', roleSchema)
--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -3,8 +3,8 @@
 module.exports = (schema, dataPart) => {
   return async (req, res, next) => {
     try {
-      const validated = await schema.validateAsync(req[dataPart])
-      req[dataPart] = validated
+      const validated = await schema.validateAsync(req[`${dataPart}`])
+      req[`${dataPart}`] = validated
       next()
     } catch (err) {
       next(err)
